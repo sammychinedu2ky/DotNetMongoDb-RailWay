e3e9419cdefe39db71bf7ef5480834031426e01b
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-25 16:52:19 +0100

    return list

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ app.MapGet("/",()=>"Hello world 🥂");
 
 app.MapGet("/{name}", async (string name,IMongoCollection<Person> collection) =>
 {
-    // find users
+    // find users bearing same name
    var result = await collection
    .Find(Builders<Person>.Filter.Eq(x=>x.name,name))
    .Project(Builders<Person>.Projection.Expression(x=>new Person(x.name,x.age)))
